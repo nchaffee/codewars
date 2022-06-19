b84083687d478ab7a13f6819708a0cf66c31304d
Nick Chaffee <nchaffee@gmail.com>
2022-06-19 09:23:20 -0400

    add sql for beginners #1

---
+++ 8kyu/sql_for_beginners_1_adults_only.sql
@@ -0,0 +1,12 @@
+-- In your application, there is a section for adults only. You need to get a list of names and ages of users from the users table, who are 18 years old or older.
+
+-- users table schema
+
+--     name
+--     age
+SELECT * FROM users WHERE age > 17;
+
+-- use aliases
+select u.name, u.age 
+from users as u
+where age >= 18
